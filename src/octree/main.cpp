d61baffe1442b63218875ba8d7450617d5104d4d
Anatole Moreau <moreau_2@epita.fr>
2017-10-30 15:31:31 +0100

    main is now nearest_bloc computator

--- src/octree/main.cpp
+++ src/octree/main.cpp
@@ -4,15 +4,15 @@
 
 void help(char* name)
 {
-    std::cout << "Usage: " << name << " depth size_x size_y size_z" << std::endl;
+    std::cout << "Usage: " << name << " depth size_x size_y size_z threshold pos_x pos_y pos_z" << std::endl;
     std::cout << "example:" << std::endl;
-    std::cout << name << " 3 100 100 100" << std::endl;
-    std::cout << "=> Splits an image of 100x100x100 into 3^8 = 512 blocks" << std::endl;
+    std::cout << name << " 3 100 100 100 30. 110 110 110" << std::endl;
+    std::cout << "=> Splits an image of 100x100x100 into 3^8 = 512 blocks and stores the blocks near with a distance < 30. of the position (110, 110, 110)" << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc != 5)
+    if (argc != 9)
     {
         help(argv[0]);
         exit(0);
@@ -23,14 +23,17 @@ int main(int argc, char* argv[])
     std::cout << "Size: " << octMap->size() << std::endl;
     std::cout << "Depth: " << octMap->depth() << std::endl;
     std::cout << "Leaves: " << octMap->leaves() << std::endl;
-    std::cout << "First floor: " << std::endl;
-    for (int i = 0; i < 8; ++i)
+    std::vector<ImageBlock*> near_blocks;
+    float threshold = std::stof(argv[5]);
+    float position[3] = {std::stof(argv[6]), std::stof(argv[7]), std::stof(argv[8])};
+    get_near_blocks(near_blocks, octMap, position, threshold);
+    for (int i = 0; i < near_blocks.size(); ++i)
     {
         for (int j = 0; j < 3; ++j)
-            std::cout << octMap->get_child(i)->get_node()->position[j] << " ";
+            std::cout << near_blocks[i]->position[j] << " ";
         std::cout << std::endl;
         for (int j = 0; j < 3; ++j)
-            std::cout << octMap->get_child(i)->get_node()->size[j] << " ";
+            std::cout << near_blocks[i]->size[j] << " ";
         std::cout << std::endl;
         std::cout << std::endl;
     }
