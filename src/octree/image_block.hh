e1bd547705790697b8b3f94484d00fce284ffa74
Anatole Moreau <moreau_2@epita.fr>
2017-10-30 15:31:07 +0100

    add nearest_bloc computation from a position and a threshold; positions are now floats

--- src/octree/image_block.hh
+++ src/octree/image_block.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "octree.hh"
+#include <cmath>
+#include <vector>
 
 class ImageBlock
 {
@@ -8,12 +10,15 @@ class ImageBlock
         ImageBlock();
         virtual ~ImageBlock();
     public:
+        float distance(float position[3]);
         int depth;
-        int position[3];
+        float position[3];
         int size[3];
+        bool generated;
         static const float vector[8][3];
 };
 
 void build_image_octree(Octree<ImageBlock>* octMap, const int tree_depth, int vol_dim[3]);
 void add_image_blocks(Octree<ImageBlock>* octMap, const int tree_depth);
 Octree<ImageBlock>* add_image_block(Octree<ImageBlock>* octMap, int idx);
+void get_near_blocks(std::vector<ImageBlock*>& res, Octree<ImageBlock>* octMap, float position[3], float threshold);
