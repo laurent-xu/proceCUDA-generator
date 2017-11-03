f7ba0df65dc5144f8c8ca6e8be0c653a29c03e52
Laurent XU <laurent.xu@epita.fr>
2017-11-04 00:49:18 +0100

    wip

--- src/density/F3Grid.hh
+++ src/density/F3Grid.hh
@@ -1,9 +1,10 @@
 #pragma once
-#define GRIDSIZE 32
+#include <utils/cudamacro.hh>
 #include <glm/glm.hpp>
 #include <cassert>
 #include <memory>
 #include <vector>
+#include <cstdlib>
 
 struct F3
 {
@@ -14,34 +15,20 @@ struct F3
   vec3_t grad;
 };
 
-class GridF3;
-
-class GridF3Data
+struct GridInfo
 {
-public:
-  using grid_t = std::shared_ptr<GridF3Data>;
-  using const_grid_t = std::shared_ptr<const GridF3Data>;
-
-  static grid_t get_grid()
+  using dist_t = double;
+  using vec3_t = glm::tvec3<dist_t>;
+  GridInfo(dist_t precision, vec3_t offset, size_t dimension)
+    : precision(precision),
+      offset(offset),
+      dimension(dimension)
   {
-    if (!free_.empty())
-    {
-      auto result = free_.back();
-      free_.pop_back();
-      return result;
-    }
-    return std::make_shared<GridF3Data>();
   }
 
-  static void release(const grid_t& data)
-  {
-    free_.push_back(data);
-  }
-
-private:
-  friend class GridF3;
-  static std::vector<grid_t> free_;
-  F3 points_[GRIDSIZE][GRIDSIZE][GRIDSIZE];
+  dist_t precision;
+  vec3_t offset;
+  size_t dimension;
 };
 
 class GridF3
@@ -49,48 +36,83 @@ class GridF3
 public:
   using dist_t = F3::val_t;
   using vec3_t = F3::vec3_t;
-  using grid_t = GridF3Data::grid_t;
-  using const_grid_t = GridF3Data::const_grid_t;
+  using grid_t = std::shared_ptr<GridF3>;
 
-  GridF3(const dist_t& precision, const vec3_t& offset)
-    : data_(GridF3Data::get_grid()),
-      precision_(precision),
-      offset_(offset) {}
-
-  ~GridF3()
+#ifdef CUDA_GENERATION
+  __device__ GridF3(const dist_t& precision,
+                    const vec3_t& offset,
+                    size_t dimension)
+    : info_(precision, offset, dimension)
   {
-    GridF3Data::release(data_);
+    points_ = malloc(sizeof(F3) * dimension * dimension * dimension);
   }
 
-  size_t dim_size() const { return GRIDSIZE; }
-
-  F3& at(size_t x, size_t y, size_t z)
+  __device__ GridF3(const GridInfo& info)
+    : info_(info)
   {
-    return data_->points_[x][y][z];
+    auto dimension = info.dimension;
+    points_ = malloc(sizeof(F3) * dimension * dimension * dimension);
   }
 
-  F3 at(size_t x, size_t y, size_t z) const
+  __device__ GridF3(const GridInfo& info)
+    : info_(info)
   {
-    return data_->points_[x][y][z];
+    free(points_);
+  }
+#endif
+
+  template <typename... Args>
+  static HOST_TARGET_GENERATION grid_t get_grid(Args&&... args)
+  {
+    return std::make_shared<GridF3>(new GridF3(std::forward(args)...));
   }
 
-  vec3_t to_position(size_t x, size_t y, size_t z) const
+  HOST_TARGET_GENERATION ~GridF3()
   {
-    return offset_ + vec3_t(x, y, z) * precision_;
+    HOST_FREE_GENERATION(points_);
   }
 
-  grid_t get_grid()
+  BOTH_TARGET_GENERATION size_t dim_size() const { return info_.dimension; }
+
+  DEVICE_TARGET_GENERATION F3& at(size_t x, size_t y, size_t z)
   {
-    return data_;
+    return points_[x * info_.dimension * info_.dimension +
+                   y * info_.dimension + z];
   }
 
-  const_grid_t get_grid() const
+  BOTH_TARGET_GENERATION vec3_t to_position(size_t x, size_t y, size_t z) const
   {
-    return std::const_pointer_cast<const GridF3Data>(data_);
+    return info_.offset + vec3_t(x, y, z) * info_.precision;
+  }
+
+  BOTH_TARGET_GENERATION F3* get_grid()
+  {
+    return points_;
+  }
+
+  BOTH_TARGET_GENERATION GridInfo get_grid_info()
+  {
+    return info_;
   }
 
 private:
-  GridF3Data::grid_t data_;
-  dist_t precision_;
-  vec3_t offset_;
+  HOST_TARGET_GENERATION GridF3(const dist_t& precision,
+                                const vec3_t& offset,
+                                size_t dimension)
+    : info_(precision, offset, dimension)
+  {
+    HOST_MALLOC_GENERATION(points_,
+                           sizeof(F3) * dimension * dimension * dimension);
+  }
+
+  HOST_TARGET_GENERATION GridF3(const GridInfo& info)
+    : info_(info)
+  {
+    auto dimension = info.dimension;
+    HOST_MALLOC_GENERATION(points_,
+                           sizeof(F3) * dimension * dimension * dimension);
+  }
+
+  F3* points_;
+  const GridInfo info_;
 };
