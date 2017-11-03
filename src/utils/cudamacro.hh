f5b63ec8b179669f530622224db4fb5ab2fb98b0
Laurent XU <laurent.xu@epita.fr>
2017-11-04 00:49:18 +0100

    wip

---
+++ src/utils/cudamacro.hh
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifdef CUDA_GENERATION
+#define BOTH_TARGET_GENERATION __device__ __host__
+#define DEVICE_TARGET_GENERATION __device__
+#define HOST_TARGET_GENERATION __host__
+#define HOST_MALLOC_GENERATION(ptr, size) do {\
+    cudamalloc((void**)&ptr, size);           \
+  } while(0);
+#define HOST_FREE_GENERATION(ptr) do {\
+    cudafree((ptr));                  \
+  } while(0);
+#else
+#define BOTH_TARGET_GENERATION /**/
+#define DEVICE_TARGET_GENERATION /**/
+#define HOST_TARGET_GENERATION /**/
+#define HOST_MALLOC_GENERATION(ptr, size) do {\
+    ptr = (decltype (ptr))malloc((size));     \
+  } while(0);
+#define HOST_FREE_GENERATION(ptr) do {\
+    free(ptr);                        \
+  } while(0);
+#endif
