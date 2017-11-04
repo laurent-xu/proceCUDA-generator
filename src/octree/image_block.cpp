9796272a6f81312bb60df5fa63edb27f35ecd541
Anatole Moreau <moreau_2@epita.fr>
2017-11-04 21:34:56 +0100

    add build_positionned_octree(), that builds an octree focused on a 3D position

--- src/octree/image_block.cpp
+++ src/octree/image_block.cpp
@@ -3,17 +3,15 @@
 ImageBlock::ImageBlock()
 {
     this->depth = 0;
+    this->size = 0.;
     for (int i = 0; i < 3; ++i)
-    {
         this->position[i] = 0.;
-        this->size[i] = 0;
-    }
     this->generated = false;
 }
 
 ImageBlock::~ImageBlock() {}
 
-const float ImageBlock::vector[8][3] =
+const double ImageBlock::vector[8][3] =
 {
     {0, 0, 0},
     {0.5, 0, 0},
@@ -25,12 +23,38 @@ const float ImageBlock::vector[8][3] =
     {0.5, 0.5, 0.5},
 };
 
-void build_image_octree(Octree<ImageBlock>* octMap, const int tree_depth, int vol_dim[3])
+void build_positionned_octree(Octree<ImageBlock>* octMap, const int tree_depth, double volume, double position[3])
 {
     auto node = new ImageBlock();
     octMap->set_node(node);
-    for (int i = 0; i < 3; i++)
-        node->size[i] = vol_dim[i];
+    node->size = volume;
+    auto current_tree = octMap;
+    for (int i = 0; i < tree_depth; ++i)
+    {
+        add_image_blocks(current_tree, i + 1);
+        for (int j = 0; j < 8; ++j)
+        {
+            if (current_tree->get_child(j)->get_node()->contains(position))
+            {
+                current_tree = current_tree->get_child(j);
+                break;
+            }
+        }
+    }
+}
+
+bool ImageBlock::contains(double position[3])
+{
+    return position[0] >= this->position[0] && position[0] < this->position[0] + this->size
+    && position[1] >= this->position[1] && position[1] < this->position[1] + this->size
+    && position[2] >= this->position[2] && position[2] < this->position[2] + this->size;
+}
+
+void build_image_octree(Octree<ImageBlock>* octMap, const int tree_depth, double volume)
+{
+    auto node = new ImageBlock();
+    octMap->set_node(node);
+    node->size = volume;
     add_image_blocks(octMap, tree_depth);
 }
 
@@ -47,28 +71,28 @@ Octree<ImageBlock>* add_image_block(Octree<ImageBlock>* octMap, int idx)
     octMap->add_child(idx, new Octree<ImageBlock>());
     auto node = new ImageBlock();
     node->depth = parent_node->depth + 1;
+    node->size = (int)(parent_node->size / 2);
     for (int i = 0; i < 3; i++)
     {
-        node->position[i] = (parent_node->position[i] + ImageBlock::vector[idx][i] * parent_node->size[i]);
-        node->size[i] = (int)(parent_node->size[i] / 2);
+        node->position[i] = (parent_node->position[i] + ImageBlock::vector[idx][i] * parent_node->size);
     }
     octMap->get_child(idx)->set_node(node);
     return octMap->get_child(idx);
 }
 
-float ImageBlock::distance(float position[3])
+double ImageBlock::distance(double position[3])
 {
-    float median[3] = {0., 0., 0.};
-    float distance = 0.;
+    double median[3] = {0., 0., 0.};
+    double distance = 0.;
     for (int i = 0; i < 3; ++i)
     {
-        median[i] = this->position[i] + this->size[i] / 2;
+        median[i] = this->position[i] + this->size / 2;
         distance += (median[i] - position[i]) * (median[i] - position[i]);
     }
     return sqrt(distance);
 }
 
-void get_near_blocks(std::vector<ImageBlock*>& res, Octree<ImageBlock>* octMap, float position[3], float threshold)
+void get_near_blocks(std::vector<ImageBlock*>& res, Octree<ImageBlock>* octMap, double position[3], double threshold)
 {
     auto node = octMap->get_node();
     if (octMap->leaf() && node->distance(position) < threshold)
--- src/octree/main.cpp
+++ src/octree/main.cpp
@@ -4,38 +4,25 @@
 
 void help(char* name)
 {
-    std::cout << "Usage: " << name << " depth size_x size_y size_z threshold pos_x pos_y pos_z" << std::endl;
+    std::cout << "Usage: " << name << " depth size pos_x pos_y pos_z" << std::endl;
     std::cout << "example:" << std::endl;
-    std::cout << name << " 3 100 100 100 30. 110 110 110" << std::endl;
-    std::cout << "=> Splits an image of 100x100x100 into 3^8 = 512 blocks and stores the blocks near with a distance < 30. of the position (110, 110, 110)" << std::endl;
+    std::cout << name << " 5 100. 60. 60. 60." << std::endl;
+    std::cout << "=> Splits an image of 100x100x100 and stores the octree centered in the position (60, 60, 60) with a maximum depth of 5" << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc != 9)
+    if (argc != 6)
     {
         help(argv[0]);
         exit(0);
     }
+    double volume = std::stof(argv[2]);
+    double position[3] = {std::stof(argv[3]), std::stof(argv[4]), std::stof(argv[5])};
     Octree<ImageBlock>* octMap = new Octree<ImageBlock>();
-    int vol_dim[3] = {atoi(argv[2]), atoi(argv[3]), atoi(argv[4])};
-    build_image_octree(octMap, atoi(argv[1]), vol_dim);
+    build_positionned_octree(octMap, atoi(argv[1]), volume, position);
     std::cout << "Size: " << octMap->size() << std::endl;
     std::cout << "Depth: " << octMap->depth() << std::endl;
     std::cout << "Leaves: " << octMap->leaves() << std::endl;
-    std::vector<ImageBlock*> near_blocks;
-    float threshold = std::stof(argv[5]);
-    float position[3] = {std::stof(argv[6]), std::stof(argv[7]), std::stof(argv[8])};
-    get_near_blocks(near_blocks, octMap, position, threshold);
-    for (int i = 0; i < near_blocks.size(); ++i)
-    {
-        for (int j = 0; j < 3; ++j)
-            std::cout << near_blocks[i]->position[j] << " ";
-        std::cout << std::endl;
-        for (int j = 0; j < 3; ++j)
-            std::cout << near_blocks[i]->size[j] << " ";
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
     return 0;
 }
