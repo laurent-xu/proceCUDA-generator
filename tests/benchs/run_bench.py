a4d8b1c1c382a95874d780295eaafde57ff85402
Totonut7 <anatole.moreau@epita.fr>
2017-11-28 06:09:00 +0100

    benchmarks now using perlin binary, with correct metaparameters

--- tests/benchs/run_bench.py
+++ tests/benchs/run_bench.py
@@ -3,23 +3,23 @@ import time
 
 def main():
   os.chdir("../../bin/")
-  #range_voxels_per_grid = [4, 8, 16, 32, 64, 128]
-  range_voxels_per_grid = [4, 8]
-  range_voxels_total = [8 ** e for e in range(len(range_voxels_per_grid))][::-1]
+  range_voxels_per_grid = [4, 8, 16, 32, 64]
+  range_nb_grid = [8 ** e for e in range(len(range_voxels_per_grid))][::-1]
   nb_camera_position = 100
   min_position = -100
   max_position = 100
+  max_thread = 2024
 
   best_times = [0] * len(range_voxels_per_grid)
   cpu_times = [0] * len(range_voxels_per_grid)
   best_values = [(0, 0, 0)] * len(range_voxels_per_grid)
-  for it in range(len(range_voxels_per_grid)):
+  for it in range(2, len(range_voxels_per_grid)):
     print("============================")
     nb_voxels = range_voxels_per_grid[it]
     print("nb voxels per grid: %d" % nb_voxels)
     time1 = time.time()
-    os.system("./appsphere %d 0 0 0 %d 0 %d false %d %d %d" % (
-          range_voxels_per_grid[it], range_voxels_total[it], range_voxels_total[it], 
+    os.system("./appperlin %d 0 0 0 %d 0 %d false %d %d %d" % (
+          range_voxels_per_grid[it], range_nb_grid[it], range_nb_grid[it], 
           nb_camera_position, min_position, max_position))
     time2 = time.time()
     res = (time2 - time1) * 1000
@@ -32,11 +32,11 @@ def main():
     for i in range_threads:
       for j in range_threads:
         for k in range_threads:
-          if (i * j * k < 2024 and i < nb_voxels and j < nb_voxels and k < nb_voxels):
+          if (i * j * k < max_thread and i <= nb_voxels and j <= nb_voxels and k <= nb_voxels):
             time1 = time.time()
-            os.system("./cudaappsphere %d %d %d %d %d 0 %d false %d %d %d" % (
+            os.system("./cudaappperlin %d %d %d %d %d 0 %d false %d %d %d" % (
                 range_voxels_per_grid[it], i, j, k,
-                range_voxels_total[it], range_voxels_total[it],
+                range_nb_grid[it], range_nb_grid[it],
                 nb_camera_position, min_position, max_position))
             time2 = time.time()
             res = (time2 - time1) * 1000
@@ -45,9 +45,9 @@ def main():
               best_values[it] = (i, j, k)
               best_times[it] = res
 
-    print(best_values)
-    print(best_times)
-    print(cpu_times)
+  print(best_values)
+  print(best_times)
+  print(cpu_times)
 
 if __name__ == "__main__":
   main()
